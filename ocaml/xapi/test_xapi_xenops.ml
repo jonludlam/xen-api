898dfbff42dc55c4d2cfdf18c74fe90fae61f2d5
Jon Ludlam <jonathan.ludlam@citrix.com>
2017-11-02 15:09:01 +0000

    CA-271525: Move over all uses of 'test_mode' to use the mock rpc function instead.
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- ocaml/xapi/test_xapi_xenops.ml
+++ ocaml/xapi/test_xapi_xenops.ml
@@ -5,6 +5,7 @@ open Test_vgpu_common
 module D=Debug.Make(struct let name="test_xapi_xenops" end)
 open D
 
+
 let test_enabled_in_xenguest () =
   let should_raise = ["foo";"";"banana";"2"] in
   let should_be_true = ["TRUE";"tRuE";"1";"true"] in
@@ -55,9 +56,9 @@ let unsetup_simulator () =
 
 let test_xapi_restart () =
   let __context = make_test_database () in
+  Context.set_test_rpc __context (Mock_rpc.rpc __context);
   setup_simulator ();
   Debug.log_to_stdout ();
-  Helpers.test_mode := true;
   let open Xenops_interface in
   let open Xapi_xenops_queue in
   let module Client = (val make_client "simulator" : XENOPS) in
